8d179cd0fb82b40bbc35a40f9557d556b91dbcfa
Josef Froehle <github@josef-froehle.de>
2025-02-14 13:24:43 +0100

    initial deepnest-lib

--- crates/deepnest-lib/src/lib.rs
+++ crates/deepnest-lib/src/lib.rs
@@ -1,3 +1,11 @@
+#[cfg(feature = "traits")]
+pub use ::deepnest_types::traits;
+pub use ::deepnest_types::types;
+
+pub fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -6,7 +14,7 @@ pub fn add(left: u64, right: u64) -> u64 {
 mod tests {
     use super::*;
 
-    #[test]
+    #[test] 
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
